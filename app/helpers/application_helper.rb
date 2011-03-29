0b14cd2ab7ded71ad50916268d6e8f5bb9631b5e
Jeff Dewitt <JeffADewitt@gmail.com>
2011-03-29 16:10:50 -0700

    added a title helper
    Signed-off-by: Jeff Dewitt <JeffADewitt@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# Return a title on a pre-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial First_App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+
+
 end
