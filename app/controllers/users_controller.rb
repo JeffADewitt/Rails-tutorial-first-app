b0896c1056fb4bc30c0836849145480f2e18c67d
Jeff Dewitt <JeffADewitt@gmail.com>
2011-04-18 13:20:41 -0700

    finished user signup
    Signed-off-by: Jeff Dewitt <JeffADewitt@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,14 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success => "Welcome to My First App!" }
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
+
