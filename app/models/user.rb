b5fc865d83c69a60fecbebabb0c8875e055ceeaa
Jeff Dewitt <JeffADewitt@gmail.com>
2011-04-19 15:36:31 -0700

    working user signin
    Signed-off-by: Jeff Dewitt <JeffADewitt@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110409000448
+# Schema version: 20110409042445
 #
 # Table name: users
 #
@@ -9,6 +9,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -31,12 +32,16 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
-  
+
   class << self
     def authenticate(email, submitted_password)
       user = find_by_email(email)
-      return nil  if user.nil?
-      return user if user.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
     end
   end
 
@@ -59,3 +64,4 @@ class User < ActiveRecord::Base
     Digest::SHA2.hexdigest(string)
    end
 end
+
